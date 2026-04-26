47478ccdbede9640807010fbaf72d888916324e5
Pham Gia Khang <phamgiakhang290620042gmail.com>
2026-04-26 15:50:32 +0700

    Initial commit: Setup NextJS TS Tailwind and Golang Gin GORM

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-emerald-400">
+        Tailwind CSS đã sẵn sàng! 🎉
+      </h1>
+    </main>
+  );
+}

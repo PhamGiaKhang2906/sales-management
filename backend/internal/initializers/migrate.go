1e717da1b7104f8a088822606a0a08939474372d
Pham Gia Khang <phamgiakhang290620042gmail.com>
2026-05-14 18:06:25 +0700

    Auto create table in database

--- backend/internal/initializers/migrate.go
+++ backend/internal/initializers/migrate.go
@@ -1,5 +1,20 @@
 package initializers
 
+import "github.com/PhamGiaKhang2906/sales-management-backend/internal/models"
+
 func SyncDatabase() {
-	DB.AutoMigrate()
+	DB.AutoMigrate(
+		&models.Role{},
+		&models.User{},
+		&models.Employee{},
+		&models.Category{},
+		&models.Supplier{},
+		&models.Product{},
+		&models.Inventory{},
+		&models.StockLog{},
+		&models.Customer{},
+		&models.Order{},
+		&models.OrderItem{},
+		&models.Invoice{},
+	)
 }

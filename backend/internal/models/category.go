40bda478d93bba349b5ff1bd89d82e8593fba95c
Pham Gia Khang <phamgiakhang290620042gmail.com>
2026-06-07 11:49:04 +0700

    Update

--- backend/internal/models/category.go
+++ backend/internal/models/category.go
@@ -3,5 +3,8 @@ package models
 type Category struct {
 	ID       uint   `gorm:"primaryKey"`
 	Name     string `gorm:"size:100;not null;uniqueIndex"`
+	StoreID  uint   `gorm:"not null"`
+	Store    Store  `gorm:"foreignKey:StoreID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	
 	Products []Product
 }
